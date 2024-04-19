ac9a36fae475f6f3bc78e05588e91ff1ea742841
Ayush <ayushpreenza@gmail.com>
2024-04-19 18:57:16 +0530

    latest

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,23 @@
+describe('Search for Google Wiki page from Wikipedia website (Smoke)',{ testIsolation: false }, () => {
+  before(() => {
+      cy.visit('https://wikipedia.org')
+  })
+
+  it('Validate Page Title', () => {
+      cy.title().should('eq', 'Wikipedia')
+  })
+
+  it('Search for Google Wiki Page', () => {
+      cy.get('#searchInput').type('google')
+      cy.get('button[type="submit"]').click()
+  })
+
+  it('Validate Google Wiki Page has opened', () => {
+      cy.get('h1#firstHeading').contains('Google')
+      cy.title().should('eq', 'Google - Wikipedia')
+  })
+})
+
 describe('Validate login functionality', () => {
 
   it('Validate login with valid credentials', ()=>{
