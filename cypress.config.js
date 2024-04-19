c2e7398917c2ab23cd068dbe13cf44e359f32242
Ayush <ayushpreenza@gmail.com>
2024-04-19 17:57:34 +0530

    onFail

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: 'mocha-junit-reporter',
+  reporterOptions: {
+    mochaFile: 'results/test-results.xml',
+    outputs: true,
+    testCaseSwitchClassnameAndName: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
